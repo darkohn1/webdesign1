4d43e1fbb8119e7c3fe463d1c26a7abdd6f9cfc0
Darko Hadzi-Nikolov <darko@different.mk>
2021-01-04 19:15:27 +0100

    added swiper slider responsive breakpoints

--- footer.php
+++ footer.php
@@ -383,8 +383,27 @@
         var swiper = new Swiper('.swiper-container', {
             slidesPerView: 5,
             spaceBetween: 30,
-            loop: true
+            loop: true,
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 40
+                }
+            }   
         });
+
+
     </script>
     <?php } ?>
 </body>
